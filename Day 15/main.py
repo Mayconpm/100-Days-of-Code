1d6c105ee91cd2631278752c6dca82273efef612
Maycon Marciano <pm.maycon@gmail.com>
2023-08-23 02:47:11 -0300

    Day 15

--- Day 15/main.py
+++ Day 15/main.py
@@ -1,3 +1,5 @@
+from os import system
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -40,22 +42,44 @@ def generate_report(resources):
 
 def calculate_resources(beverage):
     ingredients = beverage["ingredients"]
-    resources_temp = resources
+    resources_temp = resources.copy()
     for ingredient, value in ingredients.items():
-        resources[ingredient] -= value
-        if resources[ingredient] < 0:
-            print()
-            return resources
+        resources_temp[ingredient] -= value
+        if resources_temp[ingredient] < 0:
+            print(f"Sorry there is not enough {ingredient}.")
+            return None
     else:
         return resources_temp
 
 
+def process_coins():
+    coins_value = 0
+    coins = {"quarter": 0.25, "dimes": 0.10, "nickles": 0.05, "pennies": 0.01}
+    for coin, value in coins.items():
+        coins_value += int(input(f"how many {coin}?: ")) * value
+    return coins_value
+
+
 machine_working = True
 money = 0
-
+system("cls")
 while machine_working:
-    drink = input("What would you like? (espresso/latte/cappuccino):")
-    if drink == "report":
-        print(f"{generate_report(resources)}")
+    selected_drink = input("What would you like? (espresso/latte/cappuccino):")
+    if selected_drink == "off":
+        machine_working = False
+    elif selected_drink == "report":
+        print(f"{generate_report(resources)}\n Money: ${money}")
     else:
-        resources = calculate_resources(MENU[drink])
+        drink = MENU[selected_drink]
+        resources_temp = calculate_resources(drink)
+        if resources_temp:
+            coins_value = process_coins()
+            drink_cost = drink["cost"]
+            change = coins_value - drink_cost
+            if change < 0:
+                print("Sorry that's not enough money. Money refunded.")
+            else:
+                print(f"Here is ${change :.2f} in change.")
+                money += drink_cost
+                resources = resources_temp
+                print(f"Here is your {selected_drink}☕️. Enjoy!")
