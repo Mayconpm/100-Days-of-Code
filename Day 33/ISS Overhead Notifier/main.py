8040c92c81730bcbfc7a5d4858373160d5513bd9
Mayconpm <pm.maycon@gmail.com>
2023-09-17 19:08:11 -0300

    Refactor ISS notifier logic to use a scheduler for periodic checks
    This commit refactors the ISS notifier logic to use a scheduler for periodic checks. Instead of running the code in an infinite while loop with a sleep time of 60 seconds, the code now uses a scheduler to schedule the ISS notifier function to run every 60 seconds. This improves code organization and readability, as well as makes the code more efficient by avoiding unnecessary sleep time.

--- Day 33/ISS Overhead Notifier/main.py
+++ Day 33/ISS Overhead Notifier/main.py
@@ -1,49 +1,22 @@
+import sched
 import smtplib
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import requests
 import yaml
 from dateutil import tz
 
-with open("Day 33\ISS Overhead Notifier\data.yml") as yaml_file:
+with open(r"Day 33\ISS Overhead Notifier\data.yml") as yaml_file:
     data = yaml.safe_load(yaml_file)
 
 MY_EMAIL = data["email"]
 MY_PASSWORD = data["password"]
-LATITUDE = data["latitude"]
-LONGITUDE = data["longitude"]
+RECEIVER_MAIL = data["receiver_mail"]
+MY_LATITUDE = data["latitude"]
+MY_LONGITUDE = data["longitude"]
 
-
-def is_iss_overhead():
-    response = requests.get(url="http://api.open-notify.org/iss-now.json")
-    response.raise_for_status()
-    data = response.json()
-
-    iss_latitude = float(data["iss_position"]["latitude"])
-    iss_longitude = float(data["iss_position"]["longitude"])
-
-    # Your position is within +5 or -5 degrees of the iss position.
-    if LATITUDE - 5 <= iss_latitude <= LATITUDE + 5 and LONGITUDE - 5 <= iss_longitude <= LONGITUDE + 5:
-        return True
-
-
-def is_night():
-    now = datetime.now()
-    date_time = now.strftime("%Y-%m-%d")
-    parameters = {"lat": LATITUDE, "lng": LONGITUDE, "formatted": 0, "date": date_time}
-
-    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-    response.raise_for_status()
-    data = response.json()
-
-    sunrise = convert_to_datetime(data["results"]["sunrise"])
-    sunset = convert_to_datetime(data["results"]["sunset"])
-
-    time_now = datetime.now().hour
-
-    if time_now >= sunset or time_now <= sunrise:
-        return True
+scheduler = sched.scheduler(time.time, time.sleep)
 
 
 def convert_to_datetime(date):
@@ -54,12 +27,60 @@ def convert_to_datetime(date):
     return date_converted
 
 
-while True:
-    time.sleep(60)
-    if is_iss_overhead() and is_night():
-        connection = smtplib.SMTP("__YOUR_SMTP_ADDRESS_HERE___")
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
-        )
+def get_sunrise_sunset(time_now):
+    date_time = time_now.strftime("%Y-%m-%d")
+    parameters = {
+        "lat": MY_LATITUDE,
+        "lng": MY_LONGITUDE,
+        "formatted": 0,
+        "date": date_time,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = convert_to_datetime(data["results"]["sunrise"])
+    sunset = convert_to_datetime(data["results"]["sunset"])
+
+    return sunrise, sunset
+
+
+def is_ISS_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if (iss_latitude - MY_LATITUDE) <= 5 and (iss_longitude - MY_LONGITUDE) <= 5:
+        return True
+    else:
+        return False
+
+
+def is_night():
+    time_now = datetime.now()
+    time_tomorrow = time_now + timedelta(days=1)
+    _, sunset = get_sunrise_sunset(time_now)
+    sunrise, _ = get_sunrise_sunset(time_tomorrow)
+
+    if time_now > sunset and time_now < sunrise:
+        return True
+
+
+time_now = datetime.now()
+
+
+def ISS_notifier(scheduler):
+    if is_night() and is_ISS_overhead():
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=RECEIVER_MAIL, msg="Look Up!")
+            print("Email Sent.")
+    scheduler.enter(60, 1, ISS_notifier, (scheduler,))
+
+
+scheduler.enter(1, 1, ISS_notifier, (scheduler,))
+scheduler.run()
